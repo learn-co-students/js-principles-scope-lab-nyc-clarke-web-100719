72041559f2c2a31dbf08c6ea6ae41c5cd963008d
TJBender <tiana.bender01@gmail.com>
2019-11-18 09:36:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob'; 
+var bestCustomer;
+const leastFavoriteCustomer = 'jerry';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    // customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'tim'
+}
